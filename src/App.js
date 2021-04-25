0e065e8791e5c9e07b122d48eaed7b4d3376054f
Your Name <nguyentiencuong21101999>
2021-04-25 09:10:29 +0700

    to do form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import { useState } from 'react';
 import './App.scss';
-import ColorBox from './component/ColorBox';
-
+import TodoList from './component/TodoList';
+import TodoForm from './component/TodoForm'
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "a" },
+    { id: 2, title: "b" },
+    { id: 3, title: "c" },
+  ])
+  const onTodoClick = (element) => {
+     const index =  todoList.findIndex(x => x.id === element.id);
+     if(index < 0) return;
+
+     const newTodoList = [...todoList];
+     newTodoList.splice(index,1);
+     setTodoList(newTodoList)
+  }
   return (
     <div className="app">
-        wellcom
-        <ColorBox/>
+      wellcom
+      {/* <ColorBox/> */}
+      <TodoList
+        todos={todoList}
+        onTodoClick={onTodoClick}
+      />
+      <TodoForm/>
     </div>
   );
 }
--- src/component/TodoList/index.jsx
+++ src/component/TodoList/index.jsx
@@ -2,15 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 TodoList.propTypes = {
-    todos:PropTypes.array,
-    onTodoClick:PropTypes.func
+    todos: PropTypes.array,
+    onTodoClick: PropTypes.func
 };
-
+TodoList.defaultProps = {
+    todos: [],
+    onTodoClick: null
+}
 
 function TodoList(props) {
+    const { todos, onTodoClick } = props;
+    const handleClick = (element) => {
+        if (onTodoClick) {
+            onTodoClick(element)
+        }
+    }
+
     return (
         <div>
-            
+            <ul className="todolist">
+                {
+                    todos.map(element => (
+                        <li onClick={() => { handleClick(element) }} key={element.id}>{element.title}</li>
+                    ))
+                }
+            </ul>
         </div>
     );
 }
